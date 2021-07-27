4e6ad2855ce10706f8ca2e74f55e4e1e133435b7
Harshit <gharshit19@gmail.com>
2021-07-27 16:08:02 +0530

    Hello

--- api2.py
+++ api2.py
@@ -31,32 +31,42 @@ with open("DataForAnalysis.csv", mode='r') as file:
             #var1 = "test"+str(i)
             #var2 = "temp"+str(i)
                         
-            seller_id = line[keys[0]]
-            relationship = line[keys[1]]
-            customer_id= line[keys[2]]
-            seller_jurisdiction = (line[keys[3]])
-            seller_nature= (line[keys[4]])
-            customer_jurisdiction = line[keys[5]]
-            customer_nature= line[keys[6]]
-            
             # seller_id = line[keys[0]]
-            # relationship = line[keys[3]]
+            # relationship = line[keys[1]]
             # customer_id= line[keys[2]]
-            # customer_id2= line[keys[4]]
-            # seller_jurisdiction = (line[keys[5]])
-            # seller_nature= line[keys[6]]
-            # customer_jurisdiction = line[keys[7]]
-            # customer_nature= line[keys[8]]
-            # customer_jurisdiction = line[keys[9]]
-            # customer_nature= line[keys[10]]
+            # seller_jurisdiction = (line[keys[3]])
+            # seller_nature= (line[keys[4]])
+            # customer_jurisdiction = line[keys[5]]
+            # customer_nature= line[keys[6]]
+            
+            seller_id = line[keys[0]]
+            relationship = line[keys[3]]
+            customer_id= line[keys[2]]
+            customer_id2= line[keys[4]]
+            seller_jurisdiction = (line[keys[5]])
+            seller_nature= line[keys[6]]
+            customer_jurisdiction = line[keys[7]]
+            customer_nature= line[keys[8]]
+            customer_jurisdiction = line[keys[9]]
+            customer_nature= line[keys[10]]
          
-            query= """
-                MERGE (s:seller {SellerId:seller_id, SellJurisdiction:seller_jurisdiction, SellNature:seller_nature})
-                MERGE (c:customer {CustomerId: customer_id, CustomerJurisdiction: customer_jurisdiction, CustomerNature:customer_nature})
-                MERGE (s)-[r:SellTo]->(c)
-                RETURN s,c
+            # query= """
+            #     MERGE (s:seller {SellerId:seller_id, SellJurisdiction:seller_jurisdiction, SellNature:seller_nature})
+            #     MERGE (c:customer {CustomerId: customer_id, CustomerJurisdiction: customer_jurisdiction, CustomerNature:customer_nature})
+            #     MERGE (s)-[r:SellTo]->(c)
+            #     RETURN s,c
+            # """
+            query = """
+               MERGE (s:seller {SellerId:line[0], SellJurisdiction: line[5], SellerNature:line[6]}) 
+               MERGE (c:customer1 {CustomerId:line[2], CustomerJurisdiction: line[7], CustomerNature:line[8]}) 
+               CREATE (f:customer2 {CustomerId2:line[4], CustomerJurisdiction2:line[9],CustomerNature2:line[10]}) 
+               MERGE (s)-[r1:SellTo]->(c)  
+               CREATE (c)-[r2:relation{relationship:line[3]}]->(f) 
+               WITH s,c,f
+               MATCH (f:customer2) WHERE f.CustomerId2 IS NULL DETACH DELETE (f)
+               WITH s,c,f 
+               MATCH (n) RETURN (n)
             """
-
             # query= """
             #     MERGE (s:seller {SellerId:line[0], SellJurisdiction: line[5], SellerNature:line[6]}) 
             #     MERGE (c:customer1 {CustomerId:line[2], CustomerJurisdiction: line[7], CustomerNature:line[8]}) 
